8381ef7bf51fd73120ef2b263446e5a908b4a7bc
akshay07k <akshaykumar5651t@gmail.com>
2024-07-07 09:40:03 +0530

    home ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ import HeroSection from "@/components/HeroSection";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <div>
-        hello
-      </div>
+      
       <HeroSection />
     </main>
   );

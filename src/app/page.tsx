0860e7243d59f1d242ae83743a14032c14a3529a
akshay07k <akshaykumar5651t@gmail.com>
2024-07-09 09:43:30 +0530

    designing cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import MovingCards from "@/components/MovingCards";
+import Webinar from "@/components/Webinar";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <FeaturedCourses />
       <WhyChooseUs />
       <MovingCards />
+      <Webinar />
     </main>
   );
 }
